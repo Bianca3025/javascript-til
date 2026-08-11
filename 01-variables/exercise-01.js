28d68b808953320ca47c19e8c9adcc1a57538d0c
ユーザーネーム <>
2026-08-12 00:10:48 +0900

    Exercises 01 to 04

---
+++ 01-variables/exercise-01.js
@@ -0,0 +1,11 @@
+const name = 'Bianca';
+const age = 32;
+const country = 'Japan';
+let city = 'Tokyo, Shibuya'
+
+//reassigning a variable
+city = 'Tokyo, Sendagaya'
+
+console.log(name);
+console.log(age);
+console.log(city);
---
+++ 01-variables/exercise-04.js
@@ -0,0 +1,13 @@
+// + concatenation
+const name = 'Bianca';
+const age = 32;
+const country = 'Japan';
+
+console.log('My name is ' + name + '. I am ' + age + ' years old and I live in ' + country + '.');
+
+// template literals
+
+const catName = 'Taiga';
+const catAge = 12; 
+
+console.log(`My cat's name is ${catName}, and he is ${catAge} years old.`);
